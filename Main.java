9893ce513ca3a1d50f3944923491e569e24ada58
Vinay-13531 <vinayhome40@gmail.com>
2023-11-15 14:03:43 -0500

    Thread Objective Summary Comment Added

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args){
         System.out.println("Hello World");
-
+        //We will make 100 threads and each will count to 1,000,000
 
     } 
     
